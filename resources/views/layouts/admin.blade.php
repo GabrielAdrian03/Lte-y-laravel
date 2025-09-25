655dff8f03dc86904f8708d9d9c6d76b665d77cb
GabrielAdrian03 <gabrieladrianxh@gmail.com>
2025-09-25 19:19:28 -0300

    arreglo de logos

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -165,7 +165,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('/tareas') }}" class="nav-link active">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-5" style="opacity: 0.8">
+      <img src="{{asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-5" style="opacity: 0.8">
       <span class="brand-text font-weight-light">Grupo 1</span>
     </a>
 
@@ -174,7 +174,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="{{asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Sergio Martínez</a>
